61c9e68772d7919b2d4d5aa88f0ddcb0aaa15fdf
Marcel Dao <marcelnamdao@gmail.com>
2018-08-24 11:56:47 -0700

    Initial Commit

---
+++ Number-Guesser/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+using std::cout;
+using std::endl;
+using std::cin;
+using namespace std::chrono_literals;
+using namespace std::this_thread;
+
+int main()
+{
+
+
+}
